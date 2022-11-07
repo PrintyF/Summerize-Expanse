68d09300e1f0123a6fe22fd8d6c0983ac7f313af
Printyf <felix.pinty@gmail.com>
2022-11-07 17:13:51 +0100

    Add test todos

---
+++ src/test/main.test.ts
@@ -0,0 +1,14 @@
+describe('', ()=> {
+
+    it.todo('test');
+
+    it.todo('sorted by category');
+
+    it.todo('If the amount for a category is zero, this category should not appear in the output.');
+
+    it.todo('Print output total per category per month.');
+
+    it.todo('Print total per category per month for a given category.');
+
+    it.todo('Print total per category for all (non zero) categories for a given month.');
+})
