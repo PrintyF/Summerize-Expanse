b6dc55964a9f2cb6432c9008063718bef16a98b1
Printyf <felix.pinty@gmail.com>
2022-11-08 10:23:17 +0100

    Refacto summerize

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,9 @@ export interface RecordSum {
 }
 
 export function summerize(records: Array<Record>): Array<RecordSum> {
-    if (records.length > 0 && records[0].category === 'home')
-        return [{category: 'home', totalAmount: 10}];
-    else if (records.length > 0 && records[0].category === 'cinema')
-        return [{category: 'cinema', totalAmount: 10}];
-    else if (records.length > 0 && records[0].category === 'school')
-        return [{category: 'school', totalAmount: 15}];
+    let recordsSum : Array<RecordSum> = [];
 
-    return [];
+    if (records.length > 0)
+        recordsSum.push({category: records[0].category, totalAmount: records[0].amount});
+    return recordsSum;
 }
